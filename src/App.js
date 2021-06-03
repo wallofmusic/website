f6786d3c431b8a0407dff354163852c56e5fbaf3
wallofmusic <wallofmusic@gmail.com>
2021-06-02 17:05:27 -0700

    more changes

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ import Photography from './Views/Photography';
 import { SpotifyApiContext, Artist } from 'react-spotify-api';
 
 function App() {
-  // <div classname="subjectLinks">
-  //   <nav>
-  //     <Link to="developer">Developer Projects</Link> <Link to="musician">Music</Link> 
-  //   </nav>
-  //   <Router>
-  //     <Developer path="developer"/>
-  //     <Musician path="musician" />
-    
-  //   </Router>
-  // </div>
   return (
     <SpotifyApiContext.Provider value={'BQAy86uEic0nTPTmqBtJm6GXCP-ZXmBrAFGaS6uLl3K1V2oqriN2RdqEbh00K4SLPTtnc3jTFwlsl0M1Ut0j8Vji6RUmpa8NuunZF-M8u-6OghcqBgiEtDkap0vwlE4GKEnuShMxX-rN2A'}>
     <div className="App">
@@ -26,9 +16,10 @@ function App() {
         <nav>
         <Link to='/' className="WebHeader">David Wall</Link>
         </nav>
+        <h4>Developer|Musician|Photographer|Creative</h4>
     
         <nav className='WebHeader2' >
-          <Link to='/developer'>Developer Projects</Link> | <Link to='/photographer'>Photography</Link> | <Link to="/musician">Music</Link>
+          <Link to='/developer' className='devLink'>Developer Projects</Link> | <Link to='/photographer' className='photoLink'>Photography</Link> | <Link to="/musician" className='musicLink'>Music</Link>
           
         </nav>
         </div>
--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -7,26 +7,34 @@ import LogoLink from '../Components/LogoLink';
 
 const HomeView = () => {
     return (
+        <>
         <div className="grid-container">
             <div className="Main">
                 <img src={photo}  className='MainPhoto'></img>
                 <p className='AboutMe'>A native of Southern California, David grew up heavily involved in the arts - playing saxophone, flute and 
                                         bassoon. During High School, David played in several ensembles, sang in Chambers Singers, played varsity tennis, and 
-                                        competed on the Academic Decathlon team, winning several medals at the County level. After graduating high school in 2007, David attended Riverside Community College 
+                                        competed on the Academic Decathlon team, winning several medals and helping the team win at the County level. After graduating high school in 2007, David attended Riverside Community College 
                                         where he studied jazz saxophone with Charlie Richards and bassoon with Crystal Duffy née Parker. 
-                                        In 2009, David left RCC to help write and record an album with his band, A Billion Ernies, before taking 
+                                        In 2009, David left RCC for Seattle to help write and record an album with his band, A Billion Ernies, before taking 
                                         it on 2 two-month tours around the United States. Following his return from tour, David moved to Seattle, 
-                                        where he became heavily involved in the classical music scene and photography. David eventually got his 
-                                        degree in music at the University of Washington in 2018, studying bassoon with Seth Krimsky, principal bassoonist for the Seattle Symphony. He has been principal bassoon for the Puget Sound
+                                        where he became heavily involved in the classical music scene and photography. David eventually attended the University
+                                        of Washington where he studied bassoon with Seth Krimsky, principal bassoonist for the Seattle Symphony. During his time there,
+                                        David also taught intermediate tennis.
+                                        He has been principal bassoon for the Puget Sound
                                         Symphony Orchestra for 7 years and plays regularly with other groups in Seattle. As a photographer, David's clients
-                                        include the Seattle Rock Orchestra, the University of Washington Wind Ensemble, Orchestra Seattle and Seattle Chambers Singers,
+                                        have included the Seattle Rock Orchestra, the University of Washington Wind Ensemble, Orchestra Seattle and Seattle Chambers Singers,
                                         as well as various individuals. Most recently, David completed a certificate in Full-Stack Web Development through
-                                        Coding Dojo. 
+                                        Coding Dojo and looks forward to a career in the tech industry.
                                         </p>
-                <LogoLink className="logos"/>
+                
                 
             </div>
+            
         </div>
+        <div className='logoPlacement'>
+            <LogoLink className="logos" />
+        </div>
+        </>
     )
 }
 
