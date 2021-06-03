c4846bf999946c0137b83d443cbfa192a5f14163
wallofmusic <wallofmusic@gmail.com>
2021-06-03 14:11:52 -0700

    added to main page and testing for mobile

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -26,22 +26,35 @@ const HomeView = () => {
                                         as well as various individuals. Most recently, David completed a certificate in Full-Stack Web Development through
                                         Coding Dojo and looks forward to a career in the tech industry.
                                         </p>
-                
-                
             </div>
-            
         </div>
         <div className='logoPlacement'>
             <LogoLink className="logos" />
         </div>
+        <div className="blurbSection">
         <div className='photoBlurb'>
-
+            <h3>On Photography:</h3>
+            <p>Being a creative and artistic person, yet with very little natural skill with drawing, photography was a natural
+                avenue for me to explore the natural beauty of the world around me. Looking at the world through a camera makes you realize 
+                how interesting even seemingly mundane things can be.
+            </p>
         </div>
         <div className='devBlurb'>
-
+            <h3>On Programming:</h3>
+            <p>As someone who has always been interested in the sciences and taken a decent amount of calculus and physics classes, part of me is drawn 
+                to programming for the creative problem solving and heavy foray into logic. While there are many ways to solve a problem in programming, 
+                I find it satisfying in a similar way that I find math satisfying, i.e. following certain steps to obtain a concrete result, which is not
+                something I get from my artistic and musical side.
+            </p>
         </div>
         <div className='musicBlurb'>
-
+            <h3>On Music:</h3>
+            <p>Music has been one of the most central aspects to my life, both playing and listening to it, and it has many roles in my life. 
+                Writing and performing music is a major outlet of emotion, anxiety, and just about any other feeling that is trapped in my brain, as well as being 
+                a large social cornerstone. Exploring differing types of music is hugely important to me, as it offers unique perspectives of the world from other musicians, whether 
+                it's classical, jazz, rock, choral, folk, or just about any type of music. Music can be historical, educational, encouraging, calming, or just plain fun.
+            </p>
+        </div>
         </div>
         </>
     )
