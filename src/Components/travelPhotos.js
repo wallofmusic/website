07292fd5e0322e578e3c1dd8b8b7415e01cb9cc5
wallofmusic <wallofmusic@gmail.com>
2021-06-06 21:07:38 -0700

    added China film photos to Travel

--- src/Components/travelPhotos.js
+++ src/Components/travelPhotos.js
@@ -13,6 +13,12 @@ export const travelPhotos = [
         orientation: 'horizontal',
         desc: 'Trying new food in China'
     },
+    {
+        id: 8,
+        url: 'https://lh3.googleusercontent.com/e4EwMhuEO2LI7pUinGBnFIz6-uzHYicth56yTDZdmiyzh65T_brr3h2vc8buArI4dGGmQ-BbCf__Y4B5a1irrn9wsxl4Oc5oqOB4xuf4m4rv7RlVQ7O3yTcFMNAmbNakc2Kr3-OOliaQtSpS8VLuekqQSKz633GQu899VcHpyr5CywP8OScUwgYZVnbK-I0iLoxxASNTIKFwMeHiwnw28tjVbm0NA1gawcH8USejq8i_IRT5--BrejZvl015sxhBEdlMHTah6nQaqNVAa5iDc8Dv_bBTLpdSuNmq9X3hSADLvr3oIJO55RsiPpqCJwO6BaI9lBt5fH0I6VbAlnqmCuY4SsYErJfQijbTZNEcl0WZO3TC6SoZ_clrtFnZyQquJ4ZjrGCF5_4v26UWQp9AuCjysxY6EqE9XeXO-r8rna_jHwyvy_u_IJZljkMcqo0vvO_3pP1yMy49mSxoJhtRR-816b9EXAK-aJz39FV-f_CQ-R5erm279DpuRJmzmkjVuF1u6Kz-Y06A7ZVLNODmvB453pzmCRrff3r51lfs3xIGOV1mVGcwr1_8KTPaBIjhzcKwqBeJVCs15fl1AHZRSeW9PdxNZXx2vUKWtMP--S2vBcu0BX8vYsWMNxXqHYkb7X0FMaeIiRhkXrZEcadOuwPod94n0SQR1M9wzsUoeoh7LstKUxQlTW-7ILOQuBUHpSng2Wh2MFl8CSWNe8OgfPo=w864-h1302-no?authuser=0',
+        orientation: 'vertical',
+        desc: 'Couple overlooking the Forbidden City'
+    },
    
   
     {
@@ -22,10 +28,22 @@ export const travelPhotos = [
         desc: 'Pictures on the Great Wall'
     },
     {
-        id: 2,
+        id: 9,
+        url: 'https://lh3.googleusercontent.com/ualg-WP5LumIkNpzdhcb2t5sFUL0GGnbz0fvkQGRkcf8HWgBlfh8h_Rmd_810Fv6Ygegp9tYvFexbYjZZf3Bgx7mtpzh4vQy8xXhEKZPRBbqowIAQDjTX0jLMsNeRpZBoIWQVkuMvhW5FauyDcXSZ66LT-ejccStI_ioC3zrCf6aDY5RE7Qtb3DrjGOCk01vhBeJlYWG5T0dWfZODnWPpnrLqnBC2hdZWfQ6smersKKgsvMpSLgnmMV9GzgHEw74cDe10xqj2XSzjk1BbiCBxVihiPNkUsJUxPgEGrY57mkZWWPCxME1iA3zsWt_ruFphho0uNCh-RvPnZcOJ0VcihBHvDQZG8zjMxEtgIxInEf2x4lKv_73_6HwWm6SU4y11cUlHWSkppw7QzdVQ3K2kGgfehXIYJq-85JCAbi3J0qbhnQr43e99ZYD5UxyzTSeb6tALiiN1KBWxkYitrWME43FRwpCRdU6hQy9ThaCIM9ed3MP2OF98X2RZ37WD67lupP2BPdD8s_EIx5Q-w2sqSZKz6HpLIpUrfdiF-mremYx1tfcfjySz1G8ImbjxjD2_-3DCvE3t3g24X1fDsg57Nq2dcupflaxChDyGBCmtjjRQrviy54IGFYbOVdkklMfHCJaMjz2AN-9vnEgkQl3F05SUb_YfsUovE4yQje_W23GMYHRtA9B68onxv9ZtVObbuxif6KtpfdoaadERZCFu0k=w864-h1302-no?authuser=0',
+        orientation: 'vertical',
+        desc: 'Guard of the Forbidden City'
+    },
+    {
+        id: 7,
         url: 'https://res.cloudinary.com/myveryownwebsite/image/upload/v1622780051/PhotosForWebsite/Travel/aBoat_p7tykq.jpg',
         orientation: 'vertical',
         desc: 'Boat on the Water'
+    },
+    {
+        id: 12,
+        url: 'https://lh3.googleusercontent.com/m2ythRRYy3dtcOmG2bnzbuTxZSnlo7yI5JNtO3HCSWPFchbJsPp5xySD7AU3ngNfZINlpdVKj-NGka516QozL01er-hb73-w_bGifs0-M3KXzrm_Fkhz7hXnPRLCKS4QRftZ0nrTtBWSJAKGg5TI5drNZ-aB3YaUGKV4P565IwBdxTGISGp8pTWRsa5J-qLGJw4TR-uVGAbSUpA_3wARZyCO3uvKjNQGQWDtUpWcEjxF8SPGkWepKdpKwOcc9eAuFuTOeJqjfqno89ooNpG1i-683A4Cs1svOuY7DaCK-KaGj7RX-tM3ne-MI675P54Wv3NGYOWbobM5hswn2XvjrjzoABtAqFPrf8vxkVqQCT6CeiV5tY0tNNEOdS6NwDj-rqXuU_DbP2Yc1b4xZ0ygZJeWo1XPg6RgFdOJIXgySV2pw9J4t9N6iRFO1odMXXE9Dv-89KDYSrNrA8WiUd-p3cxOVm-T49bqn90NrNf5YzzVbzA333lYqIpPvtZHpCXAJG5RRLnhrIEbq9Qm-JhVuWQTdEBFY8XyMfN2OV5BCz61mWT38uw37j607TEa0k1TtoGQSRZ0ApuJ-7VfUv6Sh6c86SsK28O6Kpf2sToSMaskNtvDKvtcAMiOI3j0J68d9BT3-aD3HMjXik1K24Ci-7sV4TETrtgRRFEiZ70OhVTcd0ms8xUhG50Pr_BvtQb2CmFzN8cHhtkAnPa3nMpcG5Y=w1964-h1302-no?authuser=0',
+        orientation: 'horizontal',
+        desc: 'Sentinel of the Great Wall'
     },
     {
         id: 3,
@@ -34,10 +52,28 @@ export const travelPhotos = [
         desc: 'UW Students in the Forbidden City'
     },
     {
+        id: 10,
+        url: 'https://lh3.googleusercontent.com/R2f-dGaL7AiEDBATCN6Kd8BEJY3aSXy3TlOe3C4MWG3fSKDsuuPBoPG_WBa2JCWrLozAyYt3SxV8NFEfbJBwMdpDM4V18IiO2R_WvGTytMl6_NRWHfS8P4p2yO0cQG1FpO96t-HfQoFiMDqBj6coj6xxzbIYXG3Cht61YZEMEJOz7qRN57v_Zeq5FRDJadBEfcFa1s_eDBB1oEt62CrOr-KZ774H5viuwUSUGmqAM16FYfxUgHwG802GjFUZc-yBWDvYYpONrgYNF28IoRbXC5cORxb-MCmMeUjsr4WlqODXGjeWJKnpcLE_butLIHHHPNTHLDfDMVrMXC5Rar_znrbZURTLX2GuUQ3i3J5PVtgXYT_cvDxdbhVclYP5dL6FHutnAOI-EK4ds_Gq4wKtbzoZz8kWjgzg8l2diLnHM7xKtxRFd9D8zrQtd_42QI179tA3A808ai-8LLoodiTT-FmYkGDbdMNE19IZaAjS9XgXUwjVyVBKqXVdLynkdeGhkLn5T-M1IcGZbUy58SRCEkhYUgDFAo9Yu27VGl68yKmIety--DRN2MuoeHtPQ8sHjAC6W5NZCK0DWXXML_DH4HIFXV2eccMkOYgva6Vafqr015bkCMWmLCYSUHWOCKdhMbDaFROxkWp8vnpB-gjotdsuBzIiS2suoqW5AWV7-IUAK50Ic32ol0f85V-PYBiIsLZh7UA6xobKsubZtn1xCjs=w864-h1302-no?authuser=0',
+        orientation: 'vertical',
+        desc: 'Puppy on the Great Wall'
+    },
+    {
         id: 5,
         url: 'https://res.cloudinary.com/myveryownwebsite/image/upload/v1622780054/PhotosForWebsite/Travel/maggieOutside_pvnltb.jpg',
         orientation: 'vertical',
         desc: 'Maggie and Mountains'
+    },
+    {
+        id: 6,
+        url: 'https://lh3.googleusercontent.com/kUpP-g8HFCbzrIKvVy2DztZ8jgoAJ8lsChpvgSvJqjE73uxfgIqSbFIbjxeWhfmVYAVRKiCj3kRG8Kzaxh0Nej4qIMwwKRxU8AhGOKLwDnZkKc0u_8OqmT5hg0MvgAZ-IN86lUG5734ASwuoEswESNCzcmaOEB2x0jG4lTk93nl2qzWn3mZjXMhW5ZW9h6nrPLRFowsCksTXkp6sWGW-v1LfBDkxDnRbRTSOxXkEqnTEfm10iUylDNvRjS1vDNgqjr-kbKrSXLYMo8abXAqylLtuzqpBGGemU_JtN3EZfb8O08hQRIdd1dNLWErAZfueN18TTXqkUaVQsPfKsuw1h9PHcFC4gQzwAc0wzQ2_dNAAQfv1sIUQRPHlon2-bki-cVAXJRrKqqlyUhJilk6IFD8SjLtgbK4tIwirmK7gCbgxS5nLoDLK_1jjlLl_OcEoShnOTdq-ADmEXsDhBbtsNgsTll9X1nrJb0bSFYhHdx5CU7QJ0y7rIMjSGNtQ6C0G_hIpMMtTyE4IJ13c5QQK1wGlGoAwbFyXIqOCLIJcHNnmKCR-o25uNPTAA0_S7SVZAGMJkwaWe-NTVxK3wkCdggzEUqgFC8dHvpHzq5R30B7Y_mhrNTqrKS5CdJrxZOVaSW4rIM6R_XgCUv-0BOrng7b5XNlXfOTyBT1jvxOQsSxtyM69lizS8Q_mb4LtC88Ly9Ub-Zbav3vynBUqU8gYNBk=w864-h1302-no?authuser=0',
+        orientation: 'horizontal',
+        desc: 'BW Forbidden City'
+    },
+    {
+        id: 11,
+        url: 'https://lh3.googleusercontent.com/KxrpbC1-1qUjqWROdEnVIy7jkYVyrwxYuwo78ePJyMa63wsPSYsS-ztiynO6WQ3ycgsMknxOTKV0_N7Z4NYOKpje49NyzfdsnkmVRPPuDohAoggnBjZME68PxHL0AzcP8_LxwxZXQlSgrYgj5f8wkogcO53tpiHjIn4EKwOBLULPiHAXMXyGMaesrMhQ90whkeXSdM26YVN7uh8sE86unI8D6xgJUqA6wAUJDM4UwFEEBYSdptOxHhvqNSBilaSZ2salv6Cfm8Hp2vbRAAzXAITtHivcqaQStrhOJEgliI14OsIsRf7pH1O_PiNUYSGwVKps68WaJbK2WgDmiQhdn67RrGY9LgUiJ1nzmSjbr3lih98_gRQDGKvh9vJxxBZxM-dFZJcjwQE6JytQBuBe1uyH_Ovzo5VVI00U4rRxPCgbp9GHfxj2W9ZJLqrKAtMOOVaOz-CuWA4OFX2sQqCRiKPIYUFx2mKAB3Ay57z_DYHxnpWtwZ0pFBdqGf1zidGTIG2uxx3PfbaAxsYPkPujv1ZdFUS4QpEcZU6yqBXwDmvTZ2UyeSu_wQLX0Fpmx0pL4IhPYA-j3UTkfxFancEFu0MwTzEoE_gpGsOFBNfd9oYyg9m26zNXAEmU0PgLbP1JYoV8ZrcYZXOt_TjuKoRwV07HpBUV3Wy41d2MHjaiVl9m54tpGUBwAVP9tHD7hhrK11zCC6NkpwXtCSfNTO8ZmUk=w1964-h1302-no?authuser=0',
+        orientation: 'horizontal',
+        desc: 'Michael Brockman on the Great Wall'
     },
     
    
